e9f865eace315ff0639005314d2c333c9bc5bee0
AmrAdelKhalil <amradelkhalil@gmail.com>
2018-11-02 13:47:32 +0200

    add mail_box.rb to third parties

---
+++ lib/third_parties/mail_box.rb
@@ -0,0 +1,16 @@
+module ThirdParties
+  module MailBox
+    BASE_URL = 'http://apilayer.net/api/check'.freeze
+    OPTIONAL_PARAMS = {access_key: '7f1b7cb904e555ccc4ff59a94a22e6f7', stmp: 1, format: 1, callback: nil, email: nil}
+    private
+
+    def check(optional_params)
+      RestClient.get("#{BASE_URL}?#{format_optional_params(optional_params)}")
+    end
+
+    def format_optional_params(optional_params)
+      OPTIONAL_PARAMS.merge!(optional_params)
+      OPTIONAL_PARAMS.map{|key, value| "#{key}=#{value}" unless value.nil? }.compact.join('&')
+    end
+  end
+end
